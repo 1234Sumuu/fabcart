3b07cf128cc2d1f1941477ee05b22415266cbdfe
1234Sumuu <sumaiya.akter.cse@ulab.edu.bd>
2021-12-21 14:08:47 -0800

    middleware set

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.frontend')
 
 @section('content')
+<?php
+use App\Http\Controllers\ProductController;
+$total=0;
+if(Session::has('user'))
+{
+ $total= ProductController::cartItem();
+ //$total = ProductController::cartItem();
+}
+
+?>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
@@ -9,21 +19,51 @@
 
 <section style="padding-top: 75px">
 <div class="container">
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
+                    <ul class="nav navbar-nav navbar-right">
+
+                        @guest
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
+                        {{-- <div class="alert alert-success" role="alert">
                             {{ session('status') }}
-                        </div>
+                        </div> --}}
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+
                     @endif
+                    @endguest
+
+
+            {{-- </ul> --}}
 
                     {{ __('You are logged in!') }}
+
                 </div>
+
             </div>
+        </ul>
         </div>
     </div>
 </div>
--- resources/views/layouts/inc/front-navbar.blade.php
+++ resources/views/layouts/inc/front-navbar.blade.php
@@ -88,8 +88,8 @@
             @endif
         @else
             <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }}
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -99,7 +99,7 @@
                         {{ __('Logout') }}
                     </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;" class="d-none">
                         @csrf
                     </form>
                 </div>
